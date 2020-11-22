e95a19a6f8c772c2daad2f88c466ae92072c23b6
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-22 17:17:45 +0200

    100/100

---
+++ Recursion and Backtracking/RecursiveArraySum/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine()?
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(CalcSum(array, 0));
+        }
+
+        private static int CalcSum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + CalcSum(array, index + 1);
+        }
+    }
+}
