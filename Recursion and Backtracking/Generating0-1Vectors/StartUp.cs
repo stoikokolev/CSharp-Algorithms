106b1cde699c9c8524f30ab77f428a691edc1e3c
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-22 18:27:20 +0200

    100/100

---
+++ Recursion and Backtracking/Generating0-1Vectors/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generating0_1Vectors
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var vector = new int[number];
+
+            Generate01(vector, 0);
+        }
+
+        private static void Generate01(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for (int number = 0; number <= 1; number++)
+            {
+                vector[index] = number;
+                Generate01(vector, index + 1);
+            }
+        }
+    }
+}
