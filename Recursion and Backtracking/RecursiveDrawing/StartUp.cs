6ab073fac371d36cdf3be2c80a8e370a2225952e
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-22 17:39:58 +0200

    100/100

---
+++ Recursion and Backtracking/RecursiveDrawing/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RecursiveDrawing
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            PrintFigure(number);
+        }
+
+        private static void PrintFigure(int number)
+        {
+            PrintStars(number);
+            PrintHashTags(1,number);
+        }
+
+        private static void PrintStars(int number)
+        {
+            if (number == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*',number));
+
+            PrintStars(number-1);
+        }
+
+        private static void PrintHashTags(int startNumber,int number)
+        {
+            if (startNumber > number)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('#',startNumber));
+
+            PrintHashTags(startNumber+1,number);
+        }
+    }
+}
---
+++ Recursion and Backtracking/RecursiveFactorial/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
