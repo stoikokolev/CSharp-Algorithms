2fb9bb8bb509b72b32f793ecaac592f3e871ede4
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-22 21:33:21 +0200

    solved with memomization

---
+++ Recursion and Backtracking/Fibonacci/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class StartUp
+    {
+        static readonly Dictionary<int, int> Memory = new Dictionary<int, int>() {
+            {1, 1},
+            {2, 1}
+        };
+
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RecursiveFibonacci(number));
+
+            static int RecursiveFibonacci(int number)
+            {
+                int result;
+
+                if (Memory.ContainsKey(number))
+                {
+                    result = Memory[number];
+                }
+                else
+                {
+                    result = RecursiveFibonacci(number - 1) + RecursiveFibonacci(number - 2);
+                    Memory[number] = result;
+                }
+                
+                return result;
+            }
+        }
+    }
+}
