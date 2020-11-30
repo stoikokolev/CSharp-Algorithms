40d64004eba27c7ea4936206c7c20c182aeac9a1
Stoyko Kolev <stoikokolev@gmail.com>
2020-11-30 11:55:05 +0200

    SelectionSort

---
+++ Sorting Algorithms/SelectionSort/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var sorted = SelectionSort(new[] { 7, 123, 6, -6, 0, 11, 1111, 1, 1, 11, 23, -2222, -3, 0 });
+
+            Console.WriteLine(string.Join(' ', sorted));
+        }
+
+        private static int[] SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                var minIndex = i;
+                var min = array[i];
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[j] < min)
+                    {
+                        min = array[j];
+                        minIndex = j;
+                    }
+                }
+
+                array[minIndex] = array[i];
+                array[i] = min;
+            }
+
+            return array;
+        }
+    }
+}
