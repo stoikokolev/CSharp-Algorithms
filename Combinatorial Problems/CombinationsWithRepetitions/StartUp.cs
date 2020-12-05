99ebaaae3e33a1247283ba9e3beec107a3572992
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-05 13:45:05 +0200

    100/100

--- Combinatorial Problems/CombinationsWithRepetitions/StartUp.cs
+++ Combinatorial Problems/CombinationsWithRepetitions/StartUp.cs
@@ -1,1 +1,38 @@
-﻿ 
+﻿using System;
+using System.Linq;
+
+namespace CombinationsWithRepetitions
+{
+    public class StartUp
+    {
+        private static string[] elements;
+        private static int repeats;
+
+        private static string[] combinations;
+
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            repeats = int.Parse(Console.ReadLine());
+
+            combinations = new string[repeats];
+
+            Combinations(0, 0);
+        }
+
+        private static void Combinations(int combinationIndex, int elementStartIndex)
+        {
+            if (combinationIndex >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(' ', combinations));
+                return;
+            }
+
+            for (int i = elementStartIndex; i < elements.Length; i++)
+            {
+                combinations[combinationIndex] = elements[i];
+                Combinations(combinationIndex + 1, i);
+            }
+        }
+    }
+}
