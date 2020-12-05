a6534d99d9a72fc46368741a59e8c436c06b678a
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-05 16:09:02 +0200

    100/100

---
+++ Recursion and Backtracking/NestedLoopsToRecursion/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NestedLoopsToRecursion
+{
+    public class StartUp
+    {
+        private static int number;
+
+        static void Main()
+        {
+            number = int.Parse(Console.ReadLine());
+
+            var vector = new int[number];
+
+            Generate01(vector, 0);
+        }
+
+        private static void Generate01(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+
+            for (int count = 1; count <= number; count++)
+            {
+                vector[index] = count;
+                Generate01(vector, index + 1);
+            }
+        }
+    }
+}
---
+++ Recursion and Backtracking/ReverseArray/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine().Split(); 
+
+            ReverseArr(0,array);
+
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        private static void ReverseArr(int left, string[] array)
+        {
+            if (left == array.Length / 2)
+            {
+                return;
+            }
+
+            var right = array.Length - 1 - left;
+            Swap(array, left, right);
+
+            ReverseArr(left+1,array);
+        }
+
+        private static void Swap(string[] array, int left,int right)
+        {
+            var temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+        }
+    }
+}
