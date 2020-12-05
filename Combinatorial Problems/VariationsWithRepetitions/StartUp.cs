0550bbc0516fe28622851fd0948be5b67d3eca31
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-05 13:44:41 +0200

    100/100

---
+++ Combinatorial Problems/VariationsWithRepetitions/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace VariationsWithRepetitions
+{
+    public class StartUp
+    {
+        private static string[] _elements;
+        private static string[] _variations;
+        private static int _repeats;
+        
+
+        static void Main()
+        {
+            _elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            _repeats = int.Parse(Console.ReadLine());
+
+            _variations = new string[_repeats];
+            
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= _variations.Length)
+            {
+                Console.WriteLine(string.Join(' ', _variations));
+                return;
+            }
+
+            for (int i = 0; i < _elements.Length; i++)
+            {
+                _variations[index] = _elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
---
+++ Combinatorial Problems/VariationsWithoutRepetitions/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace VariationsWithoutRepetitions
+{
+    public class StartUp
+    {
+        private static string[] _elements;
+        private static string[] _variations;
+        private static int _repeats;
+        private static bool[] _used;
+
+        static void Main()
+        {
+            _elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            _repeats = int.Parse(Console.ReadLine());
+
+            _variations=new string[_repeats];
+
+            _used=new bool[_elements.Length];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= _variations.Length)
+            {
+                Console.WriteLine(string.Join(' ', _variations));
+                return;
+            }
+
+            for (int i = 0; i < _elements.Length; i++)
+            {
+                if (!_used[i])
+                {
+                    _used[i] = true;
+                    _variations[index] = _elements[i];
+                    Variations(index+1);
+                    _used[i] = false;
+                }
+            }
+        }
+    }
+}
