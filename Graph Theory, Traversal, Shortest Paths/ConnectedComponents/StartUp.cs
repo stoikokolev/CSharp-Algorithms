3d94d7b7708e293b044cf740ea262256ca5c5f9a
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-06 20:54:28 +0200

    100/100

---
+++ Graph Theory, Traversal, Shortest Paths/ConnectedComponents/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConnectedComponents
+{
+    public class StartUp
+    {
+        private static Dictionary<int, List<int>> _graph;
+        private static HashSet<int> _visited;
+
+        public static void Main()
+        {
+            var nodesNumer = int.Parse(Console.ReadLine());
+
+            _graph = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < nodesNumer; i++)
+            {
+                var edges = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                    .ToList();
+                _graph.Add(i, edges);
+            }
+
+            _visited = new HashSet<int>();
+
+            foreach (var node in _graph.Keys)
+            {
+                var result = new List<int>();
+
+                Dfs(node, result);
+
+                if (result.Count > 0)
+                {
+                    Console.WriteLine($"Connected component: {string.Join(' ', result)}");
+                }
+            }
+
+        }
+
+        private static void Dfs(int node, List<int> result)
+        {
+            if (_visited.Contains(node))
+            {
+                return;
+            }
+
+            _visited.Add(node);
+
+            foreach (var child in _graph[node])
+            {
+                Dfs(child, result);
+            }
+
+            result.Add(node);
+        }
+    }
+}
---
+++ Graph Theory, Traversal, Shortest Paths/DFSAndBFS/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFSAndBFS
+{
+    public class StartUp
+    {
+        private static Dictionary<int, List<int>> _graph;
+        private static HashSet<int> _visited;
+
+        public static void Main()
+        {
+            _graph = new Dictionary<int, List<int>>()
+            {
+                {1, new List<int>{19,21,14}},
+                {19, new List<int>{7,12,31,21}},
+                {7, new List<int>{1}},
+                {12, new List<int>()},
+                {21, new List<int>{14}},
+                {31, new List<int>{21}},
+                {14, new List<int>{23,6}},
+                {23, new List<int>{21}},
+                {6, new List<int>()}
+            };
+
+            _visited = new HashSet<int>();
+
+            //foreach (var node in _graph.Keys)
+            //{
+            //    Dfs(node);
+            //}
+
+            foreach (var node in _graph.Keys)
+            {
+                if (!_visited.Contains(node))
+                {
+                    Bfs(node);
+                }
+            }
+        }
+
+        private static void Bfs(int start)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+
+            _visited.Add(start);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                Console.WriteLine(node);
+
+                foreach (var child in _graph[node])
+                {
+                    if (!_visited.Contains(child))
+                    {
+                        queue.Enqueue(child);
+                        _visited.Add(child);
+                    }
+                }
+            }
+        }
+
+        private static void Dfs(int node)
+        {
+            if (_visited.Contains(node))
+            {
+                return;
+            }
+
+            _visited.Add(node);
+
+            foreach (var child in _graph[node])
+            {
+                Dfs(child);
+            }
+
+            Console.WriteLine(node);
+        }
+    }
+}
