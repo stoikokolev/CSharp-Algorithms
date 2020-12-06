d549364e417322413bd136c7adfb39585890528f
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-06 10:38:12 +0200

    100/100

---
+++ Sorting Algorithms/BinarySearch/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearchPosition(number, array));
+        }
+
+        private static int BinarySearchPosition(int number, int[] array)
+        {
+            var startIndex = 0;
+            var endIndex = array.Length - 1;
+
+            while (startIndex <= endIndex)
+            {
+                var midIndex = (startIndex + endIndex) / 2;
+
+                if (array[midIndex] == number)
+                {
+                    return midIndex;
+                }
+
+                if (array[midIndex] < number)
+                {
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    endIndex = midIndex - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Sorting Algorithms/SumOfCoins/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfCoins
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var coins = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var sortedCoins = new SortedSet<int>(coins);
+
+            var target = int.Parse(Console.ReadLine());
+            var result = 0;
+            var sb = new StringBuilder();
+
+            while (target > 0 && sortedCoins.Count > 0)
+            {
+                var maxCoin = sortedCoins.Max;
+                sortedCoins.Remove(maxCoin);
+
+                if (maxCoin > target)
+                {
+                    continue;
+                }
+
+                var counter = target / maxCoin;
+                result += counter;
+
+                target = target - maxCoin * counter;
+                sb.AppendLine($"{counter} coin(s) with value {maxCoin}");
+            }
+
+            if (target > 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {result}");
+                Console.WriteLine(sb.ToString());
+            }
+
+        }
+    }
+}
