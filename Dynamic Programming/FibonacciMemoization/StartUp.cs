6707eab3f2cbfc9b7a3db0e82a83be97953e5ecc
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-16 21:22:44 +0200

    100/100

---
+++ Dynamic Programming/FibonacciMemoization/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciMemoization
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var memory = new Dictionary<int, long>();
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(number, memory));
+        }
+
+        private static long Fibonacci(int number, IDictionary<int, long> memory)
+        {
+
+            if (memory.ContainsKey(number))
+            {
+                return memory[number];
+            }
+
+            if (number <= 2)
+            {
+                return 1;
+            }
+            
+            var result = Fibonacci(number - 1, memory) + Fibonacci(number - 2, memory);
+            memory.Add(number, result);
+
+            return result;
+        }
+    }
+}
