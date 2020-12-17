bbf1d9e8140b99a0258330bb811cb63229e62b76
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-17 13:09:34 +0200

    100/100

---
+++ Dynamic Programming/BinomialCoefficients/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Xsl;
+
+namespace BinomialCoefficients
+{
+    public class StartUp
+    {
+        private static Dictionary<string, long> memo;
+
+        public static void Main()
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            memo=new Dictionary<string, long>();
+
+            Console.WriteLine(GetBinom(row, col));
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            var id = $"{row} {col}";
+
+            if (memo.ContainsKey(id))
+            {
+                return memo[id];
+            }
+
+            if (col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            var result = (GetBinom(row - 1, col) + GetBinom(row - 1, col - 1));
+            memo[id] = result;
+
+            return result;
+        }
+    }
+}
---
+++ Dynamic Programming/DividingPresents/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DividingPresents
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var presents =
+                Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            var sums = CalcSums(presents);
+            var presentsSum = presents.Sum();
+
+            var bobScore = FindBobScore(presentsSum, sums);
+            var alanScore = presentsSum - bobScore;
+
+            var alanPresents = GetPresents(sums, alanScore);
+            //var bobPresents = GetPresents(sums, bobScore);
+
+            PrintOutput(bobScore, alanScore, alanPresents);
+        }
+
+        private static void PrintOutput(int bobScore, int alanScore, List<int> alanPresents)
+        {
+            Console.WriteLine($"Difference: {bobScore - alanScore}");
+            Console.WriteLine($"Alan:{alanScore} Bob:{bobScore}");
+            Console.WriteLine($"Alan takes: {string.Join(" ", alanPresents)}");
+            Console.WriteLine("Bob takes the rest.");
+        }
+
+        private static List<int> GetPresents(Dictionary<int, int> sums, int target)
+        {
+            var presents = new List<int>();
+
+            while (target != 0)
+            {
+                var present = sums[target];
+                presents.Add(present);
+                target -= present;
+            }
+
+            return presents;
+        }
+
+        private static int FindBobScore(int presentsSum, Dictionary<int, int> sums)
+        {
+            var bobScore = (int)Math.Ceiling(presentsSum / 2.0);
+
+            while (!sums.ContainsKey(bobScore))
+            {
+                bobScore++;
+            }
+
+            return bobScore;
+        }
+
+        private static Dictionary<int, int> CalcSums(int[] numbers)
+        {
+            var result = new Dictionary<int, int> { { 0, 0 } };
+
+            foreach (var number in numbers)
+            {
+                var sums = result.Keys.ToArray();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + number;
+
+                    if (!result.ContainsKey(newSum))
+                    {
+                        result.Add(newSum, number);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
