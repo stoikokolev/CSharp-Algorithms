402fdb19cabe81dfcf863a6add201febcccdab37
Stoyko Kolev <stoikokolev@gmail.com>
2020-12-17 13:54:13 +0200

    100/100

---
+++ Dynamic Programming/SumWIthLimitedAmountOfCoins/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumWIthLimitedAmountOfCoins
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers =
+                Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            var sums = CalcSums(numbers);
+
+            Console.WriteLine(sums[target]);
+        }
+
+        private static Dictionary<int,int> CalcSums(int[] numbers)
+        {
+            var result = new Dictionary<int,int>{{0,1}};
+
+            foreach (var number in numbers)
+            {
+                var sums = result.Keys.ToArray();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + number;
+
+                    if (!result.ContainsKey(newSum))
+                    {
+                        result.Add(newSum,1);
+                    }
+                    else
+                    {
+                        result[newSum]++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Dynamic Programming/SumWithUnlimitedCoins/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace SumWithUnlimitedCoins
+{
+    public class StartUp
+    {
+
+        public static void Main()
+        {
+            var coins =
+                Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            var count = GetCount(coins, target);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetCount(int[] coins, int target)
+        {
+            var sums = new int[target + 1];
+            sums[0] = 1;
+
+            foreach (var coin in coins)
+            {
+                for (int sum = coin; sum < sums.Length; sum++)
+                {
+                    sums[sum] += sums[sum - coin];
+                }
+            }
+
+            return sums[target];
+        }
+    }
+}
